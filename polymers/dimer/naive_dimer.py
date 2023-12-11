a8b8512c29cad7a89e40f70ef6725320f89ad531
Sam Young <youngsam@sas.upenn.edu>
2023-12-10 20:40:18 -0800

    Fix naive dimer

--- polymers/dimer/naive_dimer.py
+++ polymers/dimer/naive_dimer.py
@@ -17,25 +17,19 @@ def dimer_naive(n, dim=2, init_n=None):
     if init_n is None:
         init_n = n
     
-    if n <= min(init_n//2, 300):
-        global naive_calls
-        naive_calls += 1
-        walk = naive_self_avoiding_walk(n, dim, RNG) #base case uses the myopic algorithm
+    if n <= 3:
+        walk = naive_self_avoiding_walk(n, dim) #base case uses the myopic algorithm
         return walk
     else:
         not_saw = 1
         while not_saw:
-            nt = RNG.integers(n//2, n)
+            # nt = RNG.integers(n//2, n)
             # nt = n//2
-            walk_1 = dimer_naive(nt,dim,RNG,init_n)  #recursive call
-            walk_2 = dimer_naive(n-nt,dim,RNG,init_n)  #recursive call
+            walk_1 = dimer_naive(n//2,dim,init_n)  #recursive call
+            walk_2 = dimer_naive(n-n//2,dim,init_n)  #recursive call
 
             walk_2 = walk_2 + walk_1[:,-1].reshape(-1,1)  #translates the second walk to the end of the first one
             walk_concat = np.hstack([walk_1, walk_2[:,1:]])  #concatenates the x coordinates
             if is_valid(*walk_concat):   #if walk obtained is SAW, stop
-                global nvalid
-                nvalid += 1
                 not_saw = 0
-            global ntries
-            ntries += 1
         return walk_concat
